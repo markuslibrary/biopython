da25801fe68585cd3f361da8222a0ca02c4afdb8
markuslibrary <markuslibrary@rockefeller.edu>
2024-02-09 08:59:14 -0500

    Update get_large_data.py

--- workshop-1/sample-data/get_large_data.py
+++ workshop-1/sample-data/get_large_data.py
@@ -4,7 +4,7 @@ import gzip
 
 path = "C:/Users/Claire/Documents/GitHub/biopython/workshop-1/sample-data/" # Change this to the path of your sample-data folder
 
-# Uncomment the line below to download the large data file. Re-comment once it is downloaded
+# Uncomment the line below to download the large data file. Re-comment once it is downloaded so you only do this once.
 # wget.download("http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/xenoMrna.fa.gz", out=path)
 
 count = 0
@@ -15,8 +15,8 @@ with gzip.open(path + "xenoMrna.fa.gz", "rt") as input_handle:
             seq_iter = iter(SeqIO.parse(input_handle, "fasta"))
             while count < 500000:
                 seq = next(seq_iter)
-                SeqIO.write(seq, output_handle, "fasta") # Write record in gb format to the new data file
-                SeqIO.write(seq, output_handle_gz, "fasta") # Write record in gb format to the new data file
+                SeqIO.write(seq, output_handle, "fasta") # Write record to unzipped .fa file
+                SeqIO.write(seq, output_handle_gz, "fasta") # Write record to zipped .fa file
                 count += 1
 
 print(count) # You should see 500000          
