b5b125d6d8500f83f7338ffb6ece0f04123c3581
markuslibrary <markuslibrary@rockefeller.edu>
2024-02-08 20:27:38 -0500

    Update to workshop 1
    Clean up repo
    Large data generator
    Edits to workshop 1 notebook

---
+++ workshop-1/sample-data/get_large_data.py
@@ -0,0 +1,28 @@
+import wget
+from Bio import SeqIO
+import gzip 
+
+# Uncomment the line below to download the large data file. Re-comment once it is downloaded
+# wget.download('http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/xenoMrna.fa.gz')
+
+# Move the zipped folder to sample-data, if it isn't already there
+
+print('I am running')
+
+count = 0
+
+path = "C:/Users/Claire/Documents/GitHub/biopython/workshop-1/sample-data/" # Change this to the path of your sample-data folder
+
+with gzip.open(path + "xenoMrna.fa.gz", "rt") as input_handle:
+    with open(path + "xenoMrna_short.fa", "w") as output_handle:
+        with gzip.open(path + "xenoMrna_short.fa.gz", "wt") as output_handle_gz:
+            seq_iter = iter(SeqIO.parse(input_handle, "fasta"))
+            while count < 500000:
+                seq = next(seq_iter)
+                SeqIO.write(seq, output_handle, "fasta") # Write record in gb format to the new data file
+                SeqIO.write(seq, output_handle_gz, "fasta") # Write record in gb format to the new data file
+                count += 1
+
+print(count) # You should see 500000          
+print("I am done")
+
